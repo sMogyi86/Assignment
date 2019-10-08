6d5c409b91f9e7e6e90aeb74b973f7c46187b285
Mate Somogyi <smogyi@gmail.com>
2019-10-08 10:00:08 +0200

    StringUtilTest

--- Assignment/Strings/StringUtil.cs
+++ Assignment/Strings/StringUtil.cs
@@ -1,22 +1,36 @@
-﻿using System.Linq;
-using System.Text;
+﻿using System;
+using System.Linq;
 
 /**
  * https://hu.wikipedia.org/wiki/Palindrom
  */
 namespace Assignment.Strings
 {
-    class StringUtil
+    /// <summary>
+    /// Checks if the string passed in is a palindrom or not.
+    /// </summary>
+    /// <param name="str"></param>
+    /// <returns>True if the passed in string is a palindrom, otherwise false.</returns>
+    public interface IStringUtil
     {
-        /// <summary>
-        /// Checks if the string passed in is a palindrom or not.
-        /// </summary>
-        /// <param name="str"></param>
-        /// <returns>True if the passed in string is a palindrom, otherwise false.</returns>
+        bool IsPalindrom(string str);
+    }
+
+    class StringUtil : IStringUtil
+    {
+        ///<inheritdoc/>
         public bool IsPalindrom(string str)
         {
-            var reverse = new StringBuilder(str).ToString().Reverse<char>().ToString();
-            return str.Equals(str);
+            if (str is null)
+                throw new ArgumentNullException(nameof(str));
+
+            return str == new String(str.ToCharArray().Reverse().ToArray());
         }
+    }
+
+    public class StringUtilFactory
+    {
+        public IStringUtil Create()
+            => new StringUtil();
     }
 }
--- NUnitTestProject1/StringUtilTest.cs
+++ NUnitTestProject1/StringUtilTest.cs
@@ -1,14 +1,35 @@
-﻿using NUnit.Framework;
+﻿using Assignment.Strings;
+using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NUnitTestProject1
 {
     [TestFixture]
     public class StringUtilTest
     {
+        private StringUtilFactory stringUtilFactory;
+        private IStringUtil stringUtil;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+            => this.stringUtilFactory = new StringUtilFactory();
+
+        [SetUp]
+        public void SetUp()
+            => this.stringUtil = this.stringUtilFactory.Create();
+
+        [Test]
+        public void IsPalindrom_NULLstr_Throws_ArgumentNullException()
+            => Assert.Throws<ArgumentNullException>(() => this.stringUtil.IsPalindrom(null));
+
+        [TestCase("", true)]
+        [TestCase("a", true)]
+        [TestCase("aba", true)]
+        [TestCase("abba", true)]
+        [TestCase("abc", false)]
+        [TestCase("abcd", false)]
+        public void IsPalindrom_DifferentValidInputs_(string str, bool expectedIsPalindrom)
+            => Assert.AreEqual(expectedIsPalindrom, this.stringUtil.IsPalindrom(str));
+
     }
 }
